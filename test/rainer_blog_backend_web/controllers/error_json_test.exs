0c5acb557c5cf2855fc81343150ee7ab845b6139
cvrain <cvraindays@outlook.com>
2025-03-27 22:10:56 +0800

    First commit

---
+++ test/rainer_blog_backend_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule RainerBlogBackendWeb.ErrorJSONTest do
+  use RainerBlogBackendWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert RainerBlogBackendWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert RainerBlogBackendWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
