1bd1f1270747639668f3ad83c7cb134f1c8d0ca0
cvrain <cvraindays@outlook.com>
2025-04-03 21:16:46 +0800

    清空项目重新来过

--- lib/rainer_blog_backend/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule RainerBlogBackend.Mailer do
-  use Swoosh.Mailer, otp_app: :rainer_blog_backend
-end
--- lib/rainer_blog_backend/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule RainerBlogBackend.Repo do
-  use Ecto.Repo,
-    otp_app: :rainer_blog_backend,
-    adapter: Ecto.Adapters.Postgres
-end
