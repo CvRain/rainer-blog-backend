88288775405132bb4615e9564bae8e642b331f9a
cvrain <cvraindays@outlook.com>
2025-04-19 21:29:08 +0800

    hello world

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+class Note{
+  constructor(public data:string[] = ['hello note']){
+
+  }
+}
+
+class Response{
+  constructor(public code:number = 200, public message:string = 'success', public data:any = null){
+
+  }
+}
+
+const app = new Elysia()
+.use(swagger())
+.get("/", ({path}) => "Hello Elysia" + path)
+.get("/hello", () => "Hello world!")
+.post("/hello", () => "Hello world!")
+.decorate("note", new Note())
+.get("/notes", ({note}) => note.data)
+.decorate("response", new Response())
+.get("/response", ({response}) => {
+  return {
+    code: response.code,
+    message: response.message,
+    data: response.data
+  };
+})
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
